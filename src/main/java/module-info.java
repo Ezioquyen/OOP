5d5c183b76006dc9ecf0e2c060cb68c01aee82df
Ezioquyen <129573763+Ezioquyen@users.noreply.github.com>
2023-05-21 22:34:43 +0700

    Test bai 1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.demo {
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
