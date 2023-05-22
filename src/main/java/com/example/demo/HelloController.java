8d2a921b11b03286c4e0668cc5071931287583b3
Ezioquyen <129573763+Ezioquyen@users.noreply.github.com>
2023-05-22 22:39:44 +0700

    Update Bai 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TreeItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import org.controlsfx.control.BreadCrumbBar;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private BreadCrumbBar<String> bread;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private BorderPane pane;
+    private TreeItem<String> tree;
+
+    @FXML
+    private void initialize() {
+
+        TreeItem<String> tree1, tree2;
+        tree = new TreeItem<>("Home");
+        tree1 = new TreeItem<>("My Course");
+        tree2 = new TreeItem<>("THI CUỐI KỲ");
+        tree.getChildren().addAll(tree1);
+        tree1.getChildren().addAll(tree2);
+        bread.setSelectedCrumb(tree2);
+    }
+
+    @FXML
+    private void btnQuestions(ActionEvent event) throws IOException {
+        Pane view = FXMLLoader.load(getClass().getResource("questionbank.fxml"));
+        pane.setCenter(view);
     }
 }
